4f0cc550eab11be4b92962d44efd472522f1e20f
Mario <57414832+MaRio0607@users.noreply.github.com>
2022-06-02 17:52:39 -0500

    db update

--- database/db.sql
+++ database/db.sql
@@ -1,18 +1,116 @@
--- to create a new database
-CREATE DATABASE crudnodejsmysql;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 02-06-2022 a las 22:40:44
+-- Versión del servidor: 5.7.36
+-- Versión de PHP: 7.4.26
 
--- to use database
-use crudnodejsmysql;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- creating a new table
-CREATE TABLE empleado (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(50) NOT NULL,
-  direccion VARCHAR(100) NOT NULL,
-  telefono VARCHAR(15)
-);
--- to show all tables
-show tables;
 
--- to describe table
-describe empleado;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crudnodejsmysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividad`
+--
+
+DROP TABLE IF EXISTS `actividad`;
+CREATE TABLE IF NOT EXISTS `actividad` (
+  `id_actividad` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre_actividad` varchar(50) NOT NULL,
+  `dia_de_actividad` varchar(50) NOT NULL,
+  `hora_actividad` varchar(50) NOT NULL,
+  `id_instructor` int(11) DEFAULT NULL,
+  `fechaActualizacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_actividad`),
+  KEY `id_instructor` (`id_instructor`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+DROP TABLE IF EXISTS `empleado`;
+CREATE TABLE IF NOT EXISTS `empleado` (
+  `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id`, `nombre`, `direccion`, `telefono`) VALUES
+(1, 'Mario Cuevas', 'Cerrada Santiago', '459350');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instructor`
+--
+
+DROP TABLE IF EXISTS `instructor`;
+CREATE TABLE IF NOT EXISTS `instructor` (
+  `id_instructor` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre_instructor` varchar(50) NOT NULL,
+  `direccion_instructor` varchar(100) NOT NULL,
+  `telefono_instructor` varchar(15) DEFAULT NULL,
+  `fechaActualizacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_instructor`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+DROP TABLE IF EXISTS `paciente`;
+CREATE TABLE IF NOT EXISTS `paciente` (
+  `id_paciente` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre_paciente` varchar(50) NOT NULL,
+  `edad` varchar(15) DEFAULT NULL,
+  `fecha_de_ingreso` date DEFAULT NULL,
+  `fecha_de_salida` date DEFAULT NULL,
+  `familiar_acargo` varchar(50) NOT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `fechaActualizacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_paciente`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registroactividad`
+--
+
+DROP TABLE IF EXISTS `registroactividad`;
+CREATE TABLE IF NOT EXISTS `registroactividad` (
+  `id_actividad` int(11) DEFAULT NULL,
+  `id_paciente` int(11) DEFAULT NULL,
+  KEY `id_actividad` (`id_actividad`),
+  KEY `id_paciente` (`id_paciente`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
